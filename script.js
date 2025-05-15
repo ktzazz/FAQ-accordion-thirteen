bc8f8e83592303a7b082388c5ca802087026f664
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-05-14 19:05:17 -0600

    transition

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() { //this one is like a 
   const questionAnswerContainers = document.querySelectorAll('.question-answer');
 
   questionAnswerContainers.forEach(container => {
-    const button = container.querySelector('.question-icon .arrow'); 
+    const button = container.querySelector('.question-icon .arrow');
     //gets directly the button .arrow from the father container, in case there's more than one .arrow outside the container in use. It's more specific
     const answer = container.querySelector('.answer');
     const iconOpen = button.querySelector('#icon-open');
@@ -12,19 +12,14 @@ document.addEventListener('DOMContentLoaded', function() { //this one is like a 
     iconClose.style.display = 'none';
 
     button.addEventListener('click', function() {
-      // alternate the visibility of the answer. If answer is not visible OR empty, 
-      // because js takes the information from the html
-      if (answer.style.display === "none" || answer.style.display === "") {
-        answer.style.display = "block";
-      } else {
-        answer.style.display = "none";
-      }
+      answer.classList.toggle('active');
+      // alternate the visibility of the answer using the 'active' class and CSS transitions
 
       // Alternate the visibility of the icons
-      if (answer.style.display !== "none") { // If the answer is visible
+      if (answer.classList.contains('active')) { // If the answer is visible (has the 'active' class)
         iconOpen.style.display = 'none';
         iconClose.style.display = 'block';
-      } else { // if the answer is not visible
+      } else { // if the answer is not visible (doesn't have the 'active' class)
         iconOpen.style.display = 'block';
         iconClose.style.display = 'none';
       }
